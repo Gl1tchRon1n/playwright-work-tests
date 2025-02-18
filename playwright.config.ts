41efe85d2f7178369d53ec5418060f0ae212c672
Robert Corona <rcorona@aafmaa.com>
2025-02-18 10:48:25 -0700

    config adjustment

--- playwright.config.ts
+++ playwright.config.ts
@@ -27,7 +27,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
      
-    headless: false, // Run tests in headed mode
+    headless: process.env.CI ? true : false, // Headless only in CI
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
 
